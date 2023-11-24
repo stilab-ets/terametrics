cc6e06b60cfd5631e9db2bbdfafcf9b9f10a11ce
unknown <gm_begoug@esi.dz>
2023-11-24 15:09:11 -0500

    rename fields and make them as final static

--- src/main/java/org/stilab/metrics/counter/block/metrics/BlockMetaInfo.java
+++ src/main/java/org/stilab/metrics/counter/block/metrics/BlockMetaInfo.java
@@ -7,13 +7,13 @@ import java.util.List;
 
 public class BlockMetaInfo {
 
-    private final String IMPACTED_BLOCK = "impacted_block_type";
-    private final String BLOCK_TYPE = "block";
-    private final String START_BLOCK = "start_block";
-    private final String END_BLOCK = "end_block";
-    private final String BLOCK_IDENTIFIER = "block_identifiers";
-    private final String BLOCK_ID = "block_id";
-    private final String BLOCK_NAME = "block_name";
+    private final static String impactedBlockType = "impacted_block_type";
+    private final static String blockType = "block";
+    private final static String startBlock = "start_block";
+    private final static String endBlock = "end_block";
+    private final static String blockIdentifiers = "block_identifiers";
+    private final static String blockId = "block_id";
+    private final static String blockName = "block_name";
 
     public String concatElementsOfList(List<String> stringList) {
       return String.join(" ", stringList);
@@ -24,27 +24,27 @@ public class BlockMetaInfo {
       BlockLabelIdentifier blockLabelIdentifier = new BlockLabelIdentifier();
       List<String> labels = blockLabelIdentifier.identifyLabelsOfBlock(identifiedBlock);
 
-      metrics.put(IMPACTED_BLOCK, this.concatElementsOfList(labels));
-      metrics.put(BLOCK_TYPE, identifiedBlock.key().value());
-      metrics.put(START_BLOCK, identifiedBlock.key().textRange().start().line());
-      metrics.put(END_BLOCK, identifiedBlock.value().textRange().end().line());
+      metrics.put(impactedBlockType, this.concatElementsOfList(labels));
+      metrics.put(blockType, identifiedBlock.key().value());
+      metrics.put(startBlock, identifiedBlock.key().textRange().start().line());
+      metrics.put(endBlock, identifiedBlock.value().textRange().end().line());
       labels.add(0, identifiedBlock.key().value());
-      metrics.put(BLOCK_IDENTIFIER, this.concatElementsOfList(labels));
+      metrics.put(blockIdentifiers, this.concatElementsOfList(labels));
 
       if (labels.size() == 3)  {
-        metrics.put(BLOCK_ID,   labels.get(1));
-        metrics.put(BLOCK_NAME, labels.get(2));
+        metrics.put(blockId,   labels.get(1));
+        metrics.put(blockName, labels.get(2));
 
       }
 
       if (labels.size() == 2) {
-        metrics.put(BLOCK_ID, "");
-        metrics.put(BLOCK_NAME, labels.get(1));
+        metrics.put(blockId, "");
+        metrics.put(blockName, labels.get(1));
       }
 
       if (labels.size() == 1) {
-        metrics.put(BLOCK_ID, "");
-        metrics.put(BLOCK_NAME, "");
+        metrics.put(blockId, "");
+        metrics.put(blockName, "");
       }
 
       return metrics;
